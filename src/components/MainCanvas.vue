8a3201e00aa6db71813d6d5d354caf6f3e66ad99
fripig <fripig@gmail.com>
2017-09-27 16:40:13 +0800

    add fabric

---
+++ src/components/MainCanvas.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+div
+    canvas#canvas(width=1200 height=960)
+    img#img1(src='static/images/路人1.png' @click="addObject('img1')")
+    img#img2(src='static/images/路人大嬸.png' @click="addObject('img2')")
+</template>
+<script>
+
+export default {
+  data(){
+      return {
+          canvas: null,
+          images: [
+
+          ]
+      }
+  },
+  methods: {
+      addObject : (id) =>{
+          var imgElement = document.getElementById(id);
+        var imgInstance = new fabric.Image(imgElement, {
+        left: 100,
+        top: 100,
+        angle: 0,
+        opacity: 1
+        });
+        this.canvas.add(imgInstance);
+      },
+  },
+  mounted: () => {
+      this.canvas = new fabric.Canvas('canvas');
+        
+  }
+}
+</script>
